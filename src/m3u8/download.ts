2e916772c72f5a1b25b1fc7d2c6d6681fb75ba65
utaware <1264051408@qq.com>
2025-01-13 00:16:22 +0800

    feat: 使用p-limit模块限制ts资源请求并发数

--- src/m3u8/download.ts
+++ src/m3u8/download.ts
@@ -3,6 +3,7 @@ import ora from 'ora'
 import picocolors from 'picocolors'
 import fs from 'fs-extra'
 import axios from 'axios'
+import pLimit from 'p-limit'
 
 function calcDownloadProgress(current: number, total: number) {
   return Math.round(current / total * 100) + '%'
@@ -15,6 +16,7 @@ export async function downloadTsFiles(tsFiles: ITSFile[], cacheDir: string) {
   const totalDownloadCount = tsFiles.length
   let currentDownloadCount = 0
 
+  const queue = pLimit(8)
   const spinner = ora()
 
   spinner.start('开始下载ts文件...')
@@ -22,24 +24,26 @@ export async function downloadTsFiles(tsFiles: ITSFile[], cacheDir: string) {
   const downloadPromiseQueue = tsFiles.map(v => {
     const { tsFileUrl, tsFileName } = v
     const tsCachePath = path.resolve(cacheDir, tsFileName)
-    return new Promise((resolve, reject) => {
-      axios({ method: 'get', url: tsFileUrl, responseType: 'stream' }).then(
-        response => {
-          const inputStream = response.data
-          const outputStream = fs.createWriteStream(tsCachePath)
-          inputStream.pipe(outputStream)
-          inputStream.on('end', () => {
-            currentDownloadCount++
-            const progress = calcDownloadProgress(currentDownloadCount, totalDownloadCount)
-            spinner.text = `当前下载进度: ${picocolors.green(progress)}`
-            resolve(tsFileUrl)
-          })
-          inputStream.on('error', () => {
-            reject(tsFileUrl)
-          })
-        }
-      ).catch(() => {
-        console.log(`${tsFileUrl}下载出错`)
+    return queue(() => {
+      return new Promise((resolve, reject) => {
+        axios({ method: 'get', url: tsFileUrl, responseType: 'stream' }).then(
+          response => {
+            const inputStream = response.data
+            const outputStream = fs.createWriteStream(tsCachePath)
+            inputStream.pipe(outputStream)
+            inputStream.on('end', () => {
+              currentDownloadCount++
+              const progress = calcDownloadProgress(currentDownloadCount, totalDownloadCount)
+              spinner.text = `当前下载进度: ${picocolors.green(progress)}`
+              resolve(tsFileUrl)
+            })
+            inputStream.on('error', () => {
+              reject(tsFileUrl)
+            })
+          }
+        ).catch(() => {
+          console.log(`${tsFileUrl}下载出错`)
+        })
       })
     })
   })
