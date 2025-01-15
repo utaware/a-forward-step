fb0c056f187cca7aaea10c349a7fcc246ec388f6
utaware <1264051408@qq.com>
2025-01-16 01:26:39 +0800

    feat: 下载方式调整

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import fs from 'fs-extra'
 import axios from 'axios'
 import download from 'download'
-import ora from 'ora'
+import pLimit from 'p-limit'
+import pico from 'picocolors'
 
 import {
   API_URL,
@@ -27,8 +28,23 @@ class SomeACG {
     return code === SUCCESS_CODE
   }
 
+  colorfull(message, type) {
+    switch (type) {
+      case 'primary':
+        return pico.blue(message)
+      case 'danger':
+        return pico.red(message)
+      case 'success':
+        return pico.green(message)
+      case 'info':
+        return pico.cyan(message)
+      default:
+        return message
+    }
+  }
+
   async init() {
-    this.spinner = ora()
+    this.limitQueue = pLimit(8)
     await fs.ensureDir(DOWNLOAD_DIR)
     await fs.ensureDir(LOCAL_CACHE_DIR)
     await this.ensureCache()
@@ -36,7 +52,7 @@ class SomeACG {
   }
 
   async ensureCache() {
-    const isExist = await fs.ensureFile(LOCAL_CACHE_PATH)
+    const isExist = await fs.exists(LOCAL_CACHE_PATH)
     if (isExist) {
       const cacheContent = await fs.readJSON(LOCAL_CACHE_PATH)
       this.localCache = cacheContent
@@ -50,25 +66,41 @@ class SomeACG {
   }
 
   async download(images) {
-    this.spinner.start('...开始下载...')
 
     const total = images.length
     let current = 0
 
-    for await (const item of images) {
-      const { file_name } = item
-      const isExist = Reflect.get(this.localCache, file_name)
-      if (isExist) {
-        continue
-      } else {
-        const downloadURL = getDownloadImageURL(file_name)
-        await download(downloadURL, DOWNLOAD_DIR)
-        current++
-        Reflect.set(this.localCache, file_name, item)
-        this.spinner.text = `...当前下载: ${current}/${total}...`
-      }
-    }
-    this.spinner.succeed('...下载完成...')
+    console.log(`当前第${this.colorfull(this.page, 'info')}次下载...`)
+
+    const queue = images.map((item) => {
+      return this.limitQueue(() => new Promise((resolve, reject) => {
+        const { file_name } = item
+        const isExist = Reflect.has(this.localCache, file_name)
+        if (isExist) {
+          resolve()
+        } else {
+          const downloadURL = getDownloadImageURL(file_name)
+          download(downloadURL, DOWNLOAD_DIR).then(() => {
+            current++
+            Reflect.set(this.localCache, file_name, item)
+            console.log(`下载完成:${
+              this.colorfull(file_name, 'success')
+            }...进度:${
+              this.colorfull(`${current}/${total}`, 'primary')
+            }`)
+            resolve()
+          }).catch((err) => {
+            console.log(`下载失败:${
+              this.colorfull(file_name, 'danger')
+            }`)
+            reject(err)
+          })
+        }
+      }))
+    })
+
+    await Promise.allSettled(queue)
+
   }
 
   async run() {
@@ -92,11 +124,13 @@ class SomeACG {
     } finally {
       await this.syncCache()
     }
-    // await this.checkContinueRun(images.length)
+
+    await this.checkContinueRun(images.length)
 
   }
 
   async checkContinueRun(count) {
+    const { pageSize } = this
     const isNotAll = pageSize === count
     if (isNotAll) {
       this.page++
