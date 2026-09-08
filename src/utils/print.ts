665fa49d187a15440e0799f508bf81d794c3ef70
utaware <1264051408@qq.com>
2026-09-08 16:31:42 +0800

    feat: 添加日志打印方法

---
+++ src/utils/print.ts
@@ -0,0 +1,23 @@
+import pc from 'picocolors'
+
+export type TLogType = 'success' | 'error' | 'primary' | 'warning'
+
+export function log(message: string, type: TLogType = 'primary') {
+  switch (type) {
+    case 'primary':
+      console.log(pc.blue(message))
+      break
+    case 'warning':
+      console.log(pc.yellow(message))
+      break
+    case 'success':
+      console.log(pc.green(message))
+      break
+    case 'error':
+      console.log(pc.red(message))
+      break
+    default:
+      console.log(pc.gray(message))
+      break
+  }
+}
