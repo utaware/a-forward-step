daa4871ff46c55570930f361877bb0446fb81db9
utaware <1264051408@qq.com>
2026-09-14 09:13:50 +0800

    feat: 调整并行为串行下载，降低下载访问频率

--- src/character/gallery/download.ts
+++ src/character/gallery/download.ts
@@ -7,6 +7,8 @@ import { impitDownloadFile, print } from '#utils'
 import { getRoleGalleryData } from './data'
 import { getCharacterGalleryDir } from './url'
 
+import type { IRoleGalleryItem } from './html'
+
 /**
  * 净化文件名中的非法字符
  */
@@ -14,32 +16,35 @@ function sanitizeFilename(filename: string): string {
   return filename.replace(/[\\/:*?"<>|]/g, '_')
 }
 
+// 下载单个画廊文件并保存到本地
+export async function dwonloadGalleryWithUrl(name: string, item: IRoleGalleryItem) {
+  const { downloadUrl, category, subCategory, title } = item
+  if (!downloadUrl) return
+
+  try {
+    const { ext } = parse(downloadUrl)
+    const baseSaveDir = getCharacterGalleryDir(name)
+    const realPath = subCategory ? join(baseSaveDir, category, subCategory) : join(baseSaveDir, category)
+
+    const fileExt = ext || '.png'
+    const safeTitle = sanitizeFilename(title)
+    const filename = `${safeTitle}${fileExt}`
+    const filePath = join(realPath, filename)
+
+    await ensureDir(realPath)
+    await impitDownloadFile(downloadUrl, filePath)
+  } catch (error) {
+    print(`Failed to download ${title}: ${(error as Error).message}`, 'error')
+  }
+}
+
 // 下载角色画廊数据并保存到本地
 export async function downloadRoleGalleryData(name: string) {
   const galleryData = await getRoleGalleryData(name)
 
-  await Promise.all(
-    galleryData.map(async item => {
-      const { downloadUrl, category, subCategory, title } = item
-      if (downloadUrl) {
-        try {
-          const { ext } = parse(downloadUrl)
-          const baseSaveDir = getCharacterGalleryDir(name)
-          const realPath = subCategory ? join(baseSaveDir, category, subCategory) : join(baseSaveDir, category)
-
-          const fileExt = ext || '.png'
-          const safeTitle = sanitizeFilename(title)
-          const filename = `${safeTitle}${fileExt}`
-          const filePath = join(realPath, filename)
-
-          await ensureDir(realPath)
-          await impitDownloadFile(downloadUrl, filePath)
-        } catch (error) {
-          print(`Failed to download ${title}: ${(error as Error).message}`, 'error')
-        }
-      }
-    })
-  )
+  for await (const item of galleryData) {
+    await dwonloadGalleryWithUrl(name, item)
+  }
 
   print(`Finished downloading gallery data for ${name}`, 'success')
 }
--- src/character/voice/download.ts
+++ src/character/voice/download.ts
@@ -7,29 +7,34 @@ import { impitDownloadFile, print } from '#utils'
 import { getRoleVoiceData } from './data'
 import { getCharacterVoiceDir } from './url'
 
+import type { IRoleVoiceItem } from './html'
+
+// 下载单个语音文件并保存到本地
+export async function downloadVoiceWithUrl(name: string, item: IRoleVoiceItem) {
+  const { downloadUrl, language, description } = item
+  if (!downloadUrl) return
+
+  try {
+    const { ext } = parse(downloadUrl)
+    const saveDir = getCharacterVoiceDir(name)
+    const realPath = `${saveDir}/${language}`
+    const filename = `${description}${ext}`
+    const filePath = join(realPath, filename)
+
+    await ensureDir(realPath)
+    await impitDownloadFile(downloadUrl, filePath)
+  } catch (error) {
+    print(`Failed to download ${description}: ${(error as Error).message}`, 'error')
+  }
+}
+
 // 下载角色语音数据并保存到本地
 export async function downloadRoleVoiceData(name: string) {
   const voiceData = await getRoleVoiceData(name)
 
-  await Promise.all(
-    voiceData.map(async item => {
-      const { downloadUrl, language, description } = item
-      if (downloadUrl) {
-        try {
-          const { ext } = parse(downloadUrl)
-          const saveDir = getCharacterVoiceDir(name)
-          const realPath = `${saveDir}/${language}`
-          const filename = `${description}${ext}`
-          const filePath = join(realPath, filename)
-
-          await ensureDir(realPath)
-          await impitDownloadFile(downloadUrl, filePath)
-        } catch (error) {
-          print(`Failed to download ${description}: ${(error as Error).message}`, 'error')
-        }
-      }
-    })
-  )
+  for await (const item of voiceData) {
+    await downloadVoiceWithUrl(name, item)
+  }
 
   print(`Finished downloading voice data for ${name}`, 'success')
 }
