46c36bf3dd426187d31ffdbd36cd605ade639995
utaware <1264051408@qq.com>
2026-09-15 10:08:57 +0800

    feat: 端口号占用问题修复

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,7 @@
 import { mkdir } from 'node:fs/promises'
 
+import getPort, { portNumbers } from 'get-port'
+
 import { audioDir, port } from '#config'
 import { clearAssetsDir } from '#utils'
 import { createApp } from './app'
@@ -8,13 +10,13 @@ async function start() {
   await mkdir(audioDir, { recursive: true })
 
   const app = createApp()
-  app.listen(port, () => {
-    console.log(`TTS debugger: http://localhost:${port}`)
+  const activePort = await getPort({ port: portNumbers(port, port + 1) })
+  app.listen(activePort, () => {
+    console.log(`Server is listening on port ${activePort}`)
   })
 
-  app.once('close', async () => {
-    await clearAssetsDir()
-  })
+  process.once('SIGINT', () => clearAssetsDir())
+  process.once('SIGTERM', () => clearAssetsDir())
 }
 
 start().catch(error => {
